cc54a3ed99b024505e41598fa6ac4260067818ca
Sanjana Raju <sanjanaraju@Sanjanas-MacBook-Pro.local>
2025-07-09 14:06:38 -0700

    changes to authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -46,13 +46,13 @@ token_info = {
 # Retrieve an access token using client_credentials.
 # If a valid token is already cached, reuse it.
 def get_token():
-    # Gets the current time --> used to check if token is still valid
+    # Gets the current time. This is used to check if token is still valid
     current_time = time.time()
 
-    # Checks if the token is not expired --> if it is, it prints a message and reuse it
+    # Checks if the token is not expired. If it is, it prints a message and reuse it
     if token_info["access_token"] and current_time < token_info["expires_at"]:
         logging.info("Reusing cached token")
-        return token_info["access_token"]
+        return token_info["access_token"], token_info["expires_at"]
 
     # If theres no valid token, it'll print to get a new one
     logging.info("Requesting new token...")
@@ -77,6 +77,7 @@ def get_token():
         data = response.json()
         access_token = data["access_token"]
         expires_in = data["expires_in"]
+        expires_at = current_time + expires_in
 
         # Save token and expiry in the token_info dictionary
         token_info["access_token"] = access_token
@@ -84,23 +85,24 @@ def get_token():
 
         # prints the confirmation and return the token
         logging.info("Token received and stored in memory")
-        return access_token
+        return access_token, expires_at
     
     # if request failed, it'll print an error 
     else:
         logging.error("Failed to retrieve token")
         logging.debug(f"Status code: {response.status_code}")
         logging.debug(f"Response body: {response.text}")
-        return None
+        return None, None
 
 # Main program entry point.
 # Gets an access token and makes a follow-up API call.
 def main():
     # Calls get_token() to either fetch or reuse an access token
-    token = get_token()
+    token, expires_at = get_token()
     if token:
         logging.info("Ready to use token:")
         logging.debug(f"Access token: {token}")
+        logging.debug(f"Token expires at: {expires_at} (epoch time)")
 
 if __name__ == "__main__":
     main()
